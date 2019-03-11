0f75a5edfaefb6982edca22a310af88376fe4ffc
dsatya <daminisatya@gmail.com>
2019-03-11 22:57:25 +0530

    Add sqlite schema

---
+++ initial-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO "tweet" ("user_id", "tweet_id", "content") VALUES (10, 10, "Hello World!");
+
---
+++ twitter-schema.sql
@@ -0,0 +1,30 @@
+-- sqlite3 database.db < twitter-schema.sql
+
+PRAGMA foreign_keys = ON;
+
+DROP TABLE if exists user;
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY autoincrement,
+  username TEXT NOT NULL,
+  password TEXT NOT NULL,
+  first_name TEXT,
+  last_name TEXT,
+  birth_date DATE,
+  CHECK (
+      length("birth_date") = 10
+  )
+);
+
+DROP TABLE if exists tweet;
+CREATE TABLE tweet (
+  id INTEGER PRIMARY KEY autoincrement,
+  user_id INTEGER,
+  tweet_id INTEGER,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  content TEXT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES user(id),
+  CHECK(
+      typeof("content") = "text" AND
+      length("content") <= 140
+  )
+);
